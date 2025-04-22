deae45629b8d06aad51e96d2e8c233c1c2322a8e
Imshelledin21 <5133739+Imshelledin21@users.noreply.github.com>
2025-04-22 07:55:42 -0500

    Init commit of the solution

---
+++ mythicalbeasts-automator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+        var baseUrl = "http://mythicalbeast.mythics.svc.cluster.local";
+        List<string> endpoints = new([
+            "/mythicalbeasts"
+        ]);
+
+        Console.WriteLine("Hello, World!");
+
+
+        while (true)
+        {
+            Console.WriteLine(baseUrl + endpoints[0]);
+
+            Thread.Sleep(1000);
+        }
+    }
+}
+
